8e98a4582ae2f97e25646b3e4bf85ec5dad4b8e5
Matthew Crist <hi@hondo.xyz>
2019-01-28 12:48:08 -0500

    Simplifies HTML a bit. Adds styling

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: Gutenburg - Alert Block
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: alert — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin URI: https://github.com/matthewcrist/gutenburg-alert-block
+ * Description: A simple alert block for the Gutenburg editor in Wordpress.
+ * Author: matthewcrist
+ * Author URI: https://hondo.xyz
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
